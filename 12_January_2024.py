e514bec28ee5e80d3ff0549dbe8aa351384b3875
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-26 17:33:05 +0530

    Update 12_January_2024.py

--- 12_January_2024.py
+++ 12_January_2024.py
@@ -59,6 +59,15 @@ Explanation:-
 This student had 2 scores to average: 100 and 80. The student's average grade is (100+80)/2 = 90. An average grade of 90 corresponds to the letter grade O, so the calculate() method should return the character'O'.
 '''
 
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
+
 class Student(Person):
     def __init__(self, firstName, lastName, idNumber, testScores):
         super().__init__(firstName, lastName, idNumber)
@@ -83,6 +92,16 @@ class Student(Person):
         if 40 <= avg < 55:
             return 'D'
         return 'T'
+
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
 
 '''       
 Input (stdin):
