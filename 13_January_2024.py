912dc5c238ee44a402a8b197db47c8f689019a0f
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-26 17:36:50 +0530

    Update 13_January_2024.py

--- 13_January_2024.py
+++ 13_January_2024.py
@@ -48,6 +48,14 @@ Price: 248
 Write MyBook class
 '''
 
+from abc import ABCMeta, abstractmethod
+class Book(object, metaclass=ABCMeta):
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author   
+    @abstractmethod
+    def display(): pass
+
 class MyBook(Book):
     def __init__(self, title, author, price):
         super().__init__(title, author)
@@ -55,6 +63,12 @@ class MyBook(Book):
 
     def display(self):
         print("Title: " + self.title + "\nAuthor: " + self.author + "\nPrice: " + str(self.price))
+
+title=input()
+author=input()
+price=int(input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
 
 '''
 Input (stdin):-
