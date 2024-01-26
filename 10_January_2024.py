9a772c6d9962ed32b1d958f048413fe00b67d89a
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-26 16:53:42 +0530

    Update 10_January_2024.py

--- 10_January_2024.py
+++ 10_January_2024.py
@@ -59,21 +59,21 @@ import random
 import re
 import sys
 
-N = int(input())
-DATA = bin(N)
-
-MAXIMUM = 0
-CURRENT = 0
-
-for num in DATA:
-    if num == '1':
-        CURRENT = CURRENT + 1
-    else:
-        MAXIMUM = max(MAXIMUM, CURRENT)
-        CURRENT = 0
-
-print(max(MAXIMUM, CURRENT))
-
+if __name__ == '__main__':
+    N = int(input().strip())
+    DATA = bin(N)
+    
+    MAXIMUM = 0
+    CURRENT = 0
+    
+    for num in DATA:
+        if num == '1':
+            CURRENT = CURRENT + 1
+        else:
+            MAXIMUM = max(MAXIMUM, CURRENT)
+            CURRENT = 0
+    print(max(MAXIMUM, CURRENT))
+ 
 '''
 Input (stdin):
 5   
