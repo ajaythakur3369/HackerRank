c2865bf000b1d8cf2ba134c717100e1351043401
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-26 17:47:34 +0530

    Update 14_January_2024.py

--- 14_January_2024.py
+++ 14_January_2024.py
@@ -46,9 +46,13 @@ To find the maximum difference, computeDifference checks each element in the arr
 The maximum of these differences is 4, so it saves the value 4 as the maximumDifference instance variable. The locked stub code in the editor then prints the value stored as maximumDifference, which is 4.
 '''
 
-'''
-Add your code here
-'''
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+     
+   	'''
+    Add your code here
+    '''
 
     def computeDifference(self):
         maximum = 0
@@ -60,6 +64,18 @@ Add your code here
                     maximum = absolute
                     
         self.maximumDifference = maximum
+
+'''
+End of Difference class
+'''
+
+_ = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)
 
 '''
 Input (stdin):- 
