b04550828e792af5ae8b1d22193437b17b993d61
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-27 10:24:11 +0530

    Update 25_January_2024.py

--- 25_January_2024.py
+++ 25_January_2024.py
@@ -52,10 +52,14 @@ from math import sqrt
 T = int(input())
 
 def isPrime(n):
+    if n < 2:
+        return "Not prime"
     for i in range(2, int(sqrt(n) + 1)):
-        if n % i is 0:
-            return False
-    return True
+        if n % i == 0:
+            return "Not prime"
+    return "Prime"
 
 for _ in range(T):
     n = int(input())
+    result = isPrime(n)
+    print(result)
