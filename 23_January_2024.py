04b2a28e6fb417a2f8a8451f74bf20ea02d1cb15
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-27 09:50:10 +0530

    Update 23_January_2024.py

--- 23_January_2024.py
+++ 23_January_2024.py
@@ -55,9 +55,28 @@ Sample Output:-
 3 2 5 1 4 7 
 '''
 
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
     def levelOrder(self, root):
         queue = [root]
-        while len(queue) is not 0:
+        while len(queue) != 0:
             curr = queue[0]
             queue = queue[1:]
             print(str(curr.data) + " ", end="")
@@ -66,6 +85,14 @@ Sample Output:-
                 queue.append(curr.left)
             if curr.right is not None:
                 queue.append(curr.right)    
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)  
                 
 '''
 Input (stdin):- 
