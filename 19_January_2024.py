dfa0773a747fbc65c3571e35e04bfc08a59c600d
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-27 09:22:51 +0530

    Update 19_January_2024.py

--- 19_January_2024.py
+++ 19_January_2024.py
@@ -53,6 +53,12 @@ class Calculator(AdvancedArithmetic):
                 s+=i
         return s
 
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
+
 '''       
 Input (stdin):-
 6
