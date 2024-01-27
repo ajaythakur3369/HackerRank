414cbf92151ef1eda879e385ef562ee023b41b6a
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-27 09:32:52 +0530

    Update 21_January_2024.py

--- 21_January_2024.py
+++ 21_January_2024.py
@@ -18,6 +18,8 @@ Output Format:-
 Your printArray function should print each element of its generic array parameter on a new line.
 '''
 
+using namespace std;
+
 '''
 Name: printArray
 Print each element of the generic vector on a new line. Do not return anything.
@@ -30,6 +32,31 @@ template<typename Element>
 void printArray(vector<Element> arr) {
     for (int i = 0; i < arr.size(); i++)
         cout << arr[i] << endl;
+}
+
+int main() {
+	int n;
+	
+	cin >> n;
+	vector<int> int_vector(n);
+	for (int i = 0; i < n; i++) {
+		int value;
+		cin >> value;
+		int_vector[i] = value;
+	}
+	
+	cin >> n;
+	vector<string> string_vector(n);
+	for (int i = 0; i < n; i++) {
+		string value;
+		cin >> value;
+		string_vector[i] = value;
+	}
+
+	printArray<int>(int_vector);
+	printArray<string>(string_vector);
+
+	return 0;
 }
 
 '''
