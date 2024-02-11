37c51b3a67c72daeda5c5dcdbe7cec0519f8d980
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-02-11 16:48:41 +0530

    Update 11_February_2024.py

--- 11_February_2024.py
+++ 11_February_2024.py
@@ -70,15 +70,10 @@ def sockMerchant(n, ar):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input().strip())
-
     ar = list(map(int, input().rstrip().split()))
-
     result = sockMerchant(n, ar)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
 
 '''
