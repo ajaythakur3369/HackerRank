6f07cc02a1c3cc7f6c994e63454c79ede479c5db
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-02-11 16:47:21 +0530

    Update 8_February_2024.py

--- 8_February_2024.py
+++ 8_February_2024.py
@@ -102,22 +102,13 @@ def twoArrays(k, A, B):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     q = int(input().strip())
-
     for q_itr in range(q):
         first_multiple_input = input().rstrip().split()
-
         n = int(first_multiple_input[0])
-
         k = int(first_multiple_input[1])
-
         A = list(map(int, input().rstrip().split()))
-
         B = list(map(int, input().rstrip().split()))
-
         result = twoArrays(k, A, B)
-
         fptr.write(result + '\n')
-
     fptr.close()
