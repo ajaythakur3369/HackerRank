118035bfe662d74f643a0d531b8437310cdeacc5
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-02-11 16:46:05 +0530

    Update 6_February_2024.py

--- 6_February_2024.py
+++ 6_February_2024.py
@@ -89,16 +89,11 @@ def countingSort(arr):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input().strip())
-
     arr = list(map(int, input().rstrip().split()))
-
     result = countingSort(arr)
-
     fptr.write(' '.join(map(str, result)))
     fptr.write('\n')
-
     fptr.close()
     
 '''
