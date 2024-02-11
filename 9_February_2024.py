43fb4dd7b6f87e88cedbdcc9d3a4e010c994d1f6
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-02-11 16:48:03 +0530

    Update 9_February_2024.py

--- 9_February_2024.py
+++ 9_February_2024.py
@@ -72,21 +72,13 @@ def birthday(s, d, m):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input().strip())
-
     s = list(map(int, input().rstrip().split()))
-
     first_multiple_input = input().rstrip().split()
-
     d = int(first_multiple_input[0])
-
     m = int(first_multiple_input[1])
-
     result = birthday(s, d, m)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
     
 '''
